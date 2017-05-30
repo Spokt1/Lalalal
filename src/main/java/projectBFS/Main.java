bf00828101c6dff6edcd01f61e09365561284a8a
Spokt1 <yurakak@yandex.ru>
2017-05-30 11:10:52 +0300

    Update Main.java

--- src/main/java/projectBFS/Main.java
+++ src/main/java/projectBFS/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
                 {0, 0, 1, 10},
-                {1, 0, 0, 1},
+                {1, 2, 0, 1},
                 {1, 2, 0, 0},
                 {0, 3, 0, 0}
         };
